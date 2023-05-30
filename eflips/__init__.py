9741780fe958d0285f69e9baad59429bdb4dc62e
Jonas <j.christiansen@tu-berlin.de>
2023-05-30 15:17:25 +0200

    Revert "Removed __init__.py because it maybe helps ith nespace packaging."
    This reverts commit 5b277a2829965fbdb76d08611fb3b4ce60e32b2e.

---
+++ eflips/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+This init file puts the components required for simulations into a single
+namespace.
+
+@author: e.lauth
+"""
+
+import eflips.settings
+from eflips.evaluation import DataLogger, DataGatherer, Evaluation
+from eflips.helperFunctions import flexprint, Tictoc, progressbar, save_json, \
+    load_json
+from eflips.settings import globalConstants, load_settings
+from eflips.simpy_ext import FilterStoreExt, PositionalStore, \
+    PositionalFilterStore, LineFilterStore, StoreConnector
